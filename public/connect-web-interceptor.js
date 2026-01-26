513066ff43f104d160707cd3d976c67c0f75f0b0
Jae-Hyeong Yang <jhyang12345@naver.com>
2026-01-26 11:17:57 +0900

    Limit payload size before messaging.

--- public/connect-web-interceptor.js
+++ public/connect-web-interceptor.js
@@ -2,6 +2,38 @@
  * Reads the message from the stream and posts it to the window.
  * This is a generator function that will be passed to the response stream.
  */
+const MAX_PAYLOAD_BYTES = 256 * 1024;
+const MAX_PREVIEW_CHARS = 2000;
+
+function _safeStringify(value) {
+  try {
+    return JSON.stringify(value);
+  } catch (error) {
+    return '"[unserializable]"';
+  }
+}
+
+function _byteLength(json) {
+  if (typeof TextEncoder !== 'undefined') {
+    return new TextEncoder().encode(json).length;
+  }
+  return json.length;
+}
+
+function _truncatePayload(value) {
+  if (value == null) return value;
+  const json = _safeStringify(value);
+  const bytes = _byteLength(json);
+  if (bytes <= MAX_PAYLOAD_BYTES) {
+    return value;
+  }
+  return {
+    __truncated: true,
+    __bytes: bytes,
+    __preview: json.slice(0, MAX_PREVIEW_CHARS),
+  };
+}
+
 async function* readMessage(req, stream) {
   for await (const m of stream) {
     if (m) {
@@ -10,8 +42,8 @@ async function* readMessage(req, stream) {
         type: "__GRPCWEB_DEVTOOLS__",
         methodType: "server_streaming",
         method: req.method.name,
-        request: req.message.toJson?.(),
-        response: resp,
+        request: _truncatePayload(req.message.toJson?.()),
+        response: _truncatePayload(resp),
       }, "*");
     }
     yield m;
@@ -31,8 +63,8 @@ const interceptor = (next) => async (req) => {
         type: "__GRPCWEB_DEVTOOLS__",
         methodType: "unary",
         method: req.method.name,
-        request: req.message.toJson(),
-        response: resp.message.toJson(),
+        request: _truncatePayload(req.message.toJson()),
+        response: _truncatePayload(resp.message.toJson()),
       }, "*")
       return resp;
     } else {
@@ -46,7 +78,7 @@ const interceptor = (next) => async (req) => {
       type: "__GRPCWEB_DEVTOOLS__",
       methodType: req.stream ? "server_streaming" : "unary",
       method: req.method.name,
-      request: req.message.toJson?.(),
+      request: _truncatePayload(req.message.toJson?.()),
       response: undefined,
       error: {
         message: e.message,
--- public/content-script.js
+++ public/content-script.js
@@ -1,6 +1,38 @@
 // Copyright (c) 2019 SafetyCulture Pty Ltd. All Rights Reserved.
 
 const injectContent = `
+const MAX_PAYLOAD_BYTES = 256 * 1024;
+const MAX_PREVIEW_CHARS = 2000;
+
+function _safeStringify(value) {
+  try {
+    return JSON.stringify(value);
+  } catch (error) {
+    return '"[unserializable]"';
+  }
+}
+
+function _byteLength(json) {
+  if (typeof TextEncoder !== 'undefined') {
+    return new TextEncoder().encode(json).length;
+  }
+  return json.length;
+}
+
+function _truncatePayload(value) {
+  if (value == null) return value;
+  const json = _safeStringify(value);
+  const bytes = _byteLength(json);
+  if (bytes <= MAX_PAYLOAD_BYTES) {
+    return value;
+  }
+  return {
+    __truncated: true,
+    __bytes: bytes,
+    __preview: json.slice(0, MAX_PREVIEW_CHARS),
+  };
+}
+
 window.__GRPCWEB_DEVTOOLS__ = function (clients) {
   if (clients.constructor !== Array) {
     return
@@ -13,14 +45,14 @@ window.__GRPCWEB_DEVTOOLS__ = function (clients) {
       type: postType,
       method,
       methodType,
-      request: request.toObject(),
+      request: _truncatePayload(request.toObject()),
     });
     stream.on('data', response => {
       window.postMessage({
         type: postType,
         method,
         methodType,
-        response: response.toObject(),
+        response: _truncatePayload(response.toObject()),
       });
       if (!!this._callbacks['data']) {
         this._callbacks['data'](response);
@@ -74,8 +106,8 @@ window.__GRPCWEB_DEVTOOLS__ = function (clients) {
             type: postType,
             method,
             methodType: "unary",
-            request: request.toObject(),
-            response: err ? undefined : response.toObject(),
+            request: _truncatePayload(request.toObject()),
+            response: err ? undefined : _truncatePayload(response.toObject()),
             error: err || undefined,
           }, "*")
           posted = true;
